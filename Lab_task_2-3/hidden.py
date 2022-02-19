1f12c4242ca9234918daa4c9bec1795b829007ca
Andrea Kozlovskyy <andrea.kozlovskyi@ucu.edu.ua>
2022-02-19 03:06:05 +0200

    initial commit

---
+++ Lab_task_2-3/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "aSIuMDBryESRpQx50s5DuxUzK",
+            "consumer_secret": "S0ZuiZQysDfp8QJ6ajUvzi5N65M97VZ4xtgcY9gIL6JYkIvgde",
+            "token_key": "1493699256628166660-UV5ijclAl9EwxMinu2dO8gKbORlCf4",
+            "token_secret": "J5PTjX0qgUnGwxDZUXF332iCeRYL4MQaNCOjwCLRlgLIc"}
